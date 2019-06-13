abc47a8be17a15404246da2ba7fb3f05ac21a8d1
Estefanía Vizcarra <estefaniavizcarra@MacBook-Air-de-Estefania-2.local>
2019-06-13 17:58:20 -0500

    apiroutes y friends

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+var friends = 
+[{
+	"name":"KoalaBear",
+	"photo":"https://upload.wikimedia.org/wikipedia/commons/3/3c/Brisbane_Koala_Bushlands_%287114372167%29.jpg",
+	"scores":[4, 3, 3, 3, 4, 3, 2]
+}, 
+{
+	"name":"Kitty",
+	"photo":"https://i.pinimg.com/736x/ba/03/23/ba03237a6d6499f0e2633314826e1526--cutest-animals-baby-animals.jpg",
+	"scores":[1, 2, 2, 2, 4, 4, 4]	
+},
+ {
+	"name":"bunny",
+	"photo":"http://www.petakids.com/save-animals/never-buy-bunnies/",
+	"scores":[3, 2, 5, 2, 4, 2, 3]
+}, 
+
+];
+
+module.exports = friends;
